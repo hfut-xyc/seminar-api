875c7b9d40c0f021ed0108c365c9de5fbe1b8eb2
hfut-xyc <595567537@qq.com>
2021-04-30 15:08:17 +0800

    first commit

---
+++ src/test/java/com/api/seminar/SeminarApplicationTests.java
@@ -0,0 +1,15 @@
+package com.api.seminar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeminarApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+        System.out.println("test");
+    }
+
+}
